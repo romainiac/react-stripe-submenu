52ef2b92ddcc229c5d05d4632d6df8a5cc1b9fd8
unknown <romanyefimets@gmail.com>
2022-02-24 09:51:34 -0800

    added sidebar and ability to open and close it

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,44 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
-
+import React from 'react';
+import logo from './images/logo.svg';
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    openSubmenu();
+  };
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img className="nav-logo" src={logo} alt="stripe" />
+          <button onClick={openSidebar} className="btn toggle-btn">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        <button className="btn signin-btn" onMouseOver={displaySubmenu}>
+          sign in
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
